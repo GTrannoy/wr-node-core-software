2f3f4dfad40a2576e7aa2ac9d268dc75fe38dfaf
Federico Vaga <federico.vaga@cern.ch>
2014-11-12 12:10:33 +0100

    tools: allow multiple slot dump
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- tools/wrnc-messages.c
+++ tools/wrnc-messages.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 #include <libwrnc.h>
 #include <getopt.h>
@@ -24,14 +25,49 @@ static void help()
 }
 
 
+void dump_message(struct wrnc_dev *wrnc, unsigned int slot_index)
+{
+	struct wrnc_msg *wmsg;
+	char str[128];
+	int j;
+
+	errno = 0;
+	wmsg = wrnc_slot_receive(wrnc, slot_index);
+	if (!wmsg) {
+		if (errno) {
+			fprintf(stderr,
+				"Cannot receive from slot %d: %s\n",
+				slot_index, strerror(errno));
+		        exit(1);
+		}
+		return;
+	}
+
+	switch (wmsg->data[0]) {
+        case 0xdeadbeef:
+		for (j = 0; j < 128 - 1 && j < wmsg->datalen; ++j)
+			str[j] = wmsg->data[j + 1];
+		str[j - 1] = '\0';
+		fprintf(stdout, "slot-%d : %s\n", slot_index, str);
+		break;
+	default:
+		fprintf(stdout, "slot-%d : unknown message\n", slot_index);
+		break;
+	}
+
+	free(wmsg);
+}
+
+#define MAX_DEV 4
+#define MAX_SLOT 32
 int main(int argc, char *argv[])
 {
-	int slot_index = 0, err;
-	unsigned int n = 0, i = 0, j;
-	uint32_t dev_id = 0;
-	char c, str[128];
-	struct wrnc_dev *wrnc;
-	struct wrnc_msg *wmsg;
+	unsigned int n = 0, i = 0, j, si = 0, di = 0, cnt = 0;
+	unsigned int slot_index[MAX_DEV][MAX_SLOT];
+	uint32_t dev_id[MAX_DEV];
+	char c;
+	struct wrnc_dev *wrnc[MAX_DEV];
+
 
 	atexit(wrnc_exit);
 
@@ -41,10 +77,17 @@ int main(int argc, char *argv[])
 			help();
 			break;
 		case 'i':
-			sscanf(optarg, "%d", &slot_index);
+			if (si >= MAX_SLOT && di > 0)
+				break;
+			sscanf(optarg, "%d", &slot_index[di - 1][si]);
+			si++;
 			break;
 		case 'D':
-			sscanf(optarg, "0x%x", &dev_id);
+			if (di >= MAX_DEV)
+				break;
+			sscanf(optarg, "0x%x", &dev_id[di]);
+			di++;
+			si = 0;
 			break;
 		case 'n':
 			sscanf(optarg, "%d", &n);
@@ -52,44 +95,33 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (!dev_id) {
-		fprintf(stderr, "Invalid wrnc device\n");
-		exit(1);
-	}
-
 	wrnc_init();
 
-	wrnc = wrnc_open_by_fmc(dev_id);
-	if (!wrnc) {
-		fprintf(stderr, "Cannot open WRNC: %s\n", wrnc_strerror(errno));
-		exit(1);
+	/* Open all devices */
+	for (i = 0; i < di; i++) {
+		wrnc[i] = wrnc_open_by_fmc(dev_id[i]);
+		if (!wrnc[i]) {
+			fprintf(stderr, "Cannot open WRNC: %s\n", wrnc_strerror(errno));
+			exit(1);
+		}
 	}
 
 	/* Get messages */
-	while(n == 0 || i < n) {
-		errno = 0;
-		wmsg = wrnc_slot_receive(wrnc, slot_index);
-		if (!wmsg) {
-			if (errno) {
-				fprintf(stderr,
-					"Cannot receive from slot %d: %s\n",
-					slot_index, strerror(errno));
-				goto out;
+	while((n == 0 || cnt < n) && (di > 0 && si > 0)) {
+		for (i = 0; i < di; i++) {
+			for (j = 0; j < si; j++) {
+				dump_message(wrnc[i], slot_index[i][j]);
+				cnt++;
+				if (n > 0 && n < cnt)
+					goto out;
 			}
-			continue;
 		}
-		i++;
-		if (wmsg->data[0] == 0xdeadbeef) {
-			for (j = 0; j < 128 - 1 && j < wmsg->datalen; ++j)
-				str[j] = wmsg->data[j + 1];
-			str[j - 1] = '\0';
-			fprintf(stdout, "SLOT-%d : %s\n", slot_index, str);
-		}
-		free(wmsg);
 	}
 
 out:
-	wrnc_close(wrnc);
+	/* Close all devices */
+	for (i = 0; i < di; i++)
+		wrnc_close(wrnc[i]);
 
 	exit(0);
 }

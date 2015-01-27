b8c619fe47cc0cd7f95adc0f647a4f9cc41c1ddf
Federico Vaga <federico.vaga@cern.ch>
2015-01-27 15:24:26 +0100

    Revert "wrtd: TDC has 5 inputs, not 2"
    WRTD_IN_MAX is not the maximum number of TDC inputs, but the
    maximum input HMQ.
    
    This reverts commit 8c761ddca2560b5d6b92e73b69e7e6cac5ab54f7.

--- applications/wrtd/include/wrtd-common.h
+++ applications/wrtd/include/wrtd-common.h
@@ -14,7 +14,7 @@
 #define WRTD_CPU_TDC 0			/* Core 0 controls the TDC mezzanine */
 #define WRTD_CPU_FD 1			/* Core 1 controls the FD mezzanine */
 
-#define WRTD_IN_MAX		5
+#define WRTD_IN_MAX		2
 #define WRTD_IN_TDC_CONTROL	0
 #define WRTD_IN_FD_CONTROL	1
 

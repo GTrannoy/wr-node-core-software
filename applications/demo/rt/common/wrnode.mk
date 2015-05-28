0294b5fff2fae376d057da27143df0a7427f5f66
Federico Vaga <federico.vaga@cern.ch>
2015-05-28 18:48:00 +0200

    demo:rt: add autodemo example (no interaction with HOST)
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

---
+++ applications/demo/rt/common/wrnode.mk
@@ -0,0 +1,27 @@
+# and don't touch the rest unless you know what you're doing.
+CROSS_COMPILE_TARGET ?= lm32-elf-
+INSTALL_PREFIX ?= .
+
+CC =		$(CROSS_COMPILE_TARGET)gcc
+LD =		$(CROSS_COMPILE_TARGET)ld
+OBJDUMP =	$(CROSS_COMPILE_TARGET)objdump
+OBJCOPY =	$(CROSS_COMPILE_TARGET)objcopy
+SIZE =		$(CROSS_COMPILE_TARGET)size
+
+CFLAGS = -DWRNODE_RT -g -O3 -I. -I../common -I../../include -mmultiply-enabled -mbarrel-shift-enabled
+OBJS += ../common/wrn-crt0.o ../common/vsprintf-xint.o ../common/printf.o ../common/rt-common.o
+LDSCRIPT = ../common/wrnode.ld
+
+all:	$(OUTPUT)
+
+$(OUTPUT): $(LDSCRIPT) $(OBJS)
+	${CC} -o $(OUTPUT).elf -nostartfiles $(OBJS) -T $(LDSCRIPT) -lgcc -lc
+	${OBJCOPY} --remove-section .smem -O binary $(OUTPUT).elf $(OUTPUT).bin
+	${OBJDUMP} -S $(OUTPUT).elf  > disasm.S
+	$(SIZE) $(OUTPUT).elf
+
+clean:
+	rm -f $(OBJS) $(OUTPUT).bin
+
+install:
+	cp $(OUTPUT).bin $(INSTALL_PREFIX)

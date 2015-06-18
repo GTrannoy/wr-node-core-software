8b09a46047d4275a497830512b53ab6cdca9ae50
Federico Vaga <federico.vaga@cern.ch>
2015-06-18 13:42:37 +0200

    wrtd:rt: remove white spaces an use tabs
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- applications/wrtd/rt/common/loop-queue.c
+++ applications/wrtd/rt/common/loop-queue.c
@@ -24,38 +24,38 @@ static SMEM struct wrtd_trigger_entry buf[16];
 
 void loop_queue_init()
 {
-    head = tail = count = 0;
+	head = tail = count = 0;
 }
 
 void loop_queue_push(struct wrtd_trig_id *id, uint32_t seq, struct wr_timestamp *ts)
 {
-    if(count == LOOP_QUEUE_SIZE)
-	return;
-	
-    buf[head].id = *id;
-    buf[head].seq = seq;
-    buf[head].ts = *ts;
-    
-    smem_atomic_add(&head, 1);
-    
-    if (head == LOOP_QUEUE_SIZE)
-	   head = 0;
-    
-    count++;
+	if(count == LOOP_QUEUE_SIZE)
+		return;
+
+	buf[head].id = *id;
+	buf[head].seq = seq;
+	buf[head].ts = *ts;
+
+	smem_atomic_add(&head, 1);
+
+	if (head == LOOP_QUEUE_SIZE)
+		head = 0;
+
+	count++;
 }
 
 struct wrtd_trigger_entry *loop_queue_pop()
 {
-    if(!count)
-        return NULL;
-    
-    struct wrtd_trigger_entry *rv = &buf[tail];
-    
-    smem_atomic_add(&tail, 1);
-    
-    if(tail == LOOP_QUEUE_SIZE)
-	   tail = 0;
-    count--;
-    
-    return rv;
+	if(!count)
+		return NULL;
+
+	struct wrtd_trigger_entry *rv = &buf[tail];
+
+	smem_atomic_add(&tail, 1);
+
+	if(tail == LOOP_QUEUE_SIZE)
+		tail = 0;
+	count--;
+
+	return rv;
 }

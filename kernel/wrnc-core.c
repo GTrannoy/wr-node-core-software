12a62112ad25004e5d448163948c32f4ca67caff
Federico Vaga <federico.vaga@cern.ch>
2015-07-24 16:22:00 +0200

    kernel: fix initialize mutex
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- kernel/wrnc-core.c
+++ kernel/wrnc-core.c
@@ -488,6 +488,7 @@ static int wrnc_probe_hmq(struct wrnc_dev *wrnc, unsigned int slot,
 	if (err)
 		return err;
 
+	mutex_init(&hmq->mtx);
 	INIT_LIST_HEAD(&hmq->list_msg_input);
 	INIT_LIST_HEAD(&hmq->list_usr);
 

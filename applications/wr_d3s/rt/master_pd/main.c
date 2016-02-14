9703967fd24a73fd3d0ca2a7e676a786f8dbaa2a
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2016-02-14 14:18:08 +0100

    wr_d3s/rt: separated slave/master. cleaning up, bug fixing

---
+++ applications/wr_d3s/rt/master_pd/main.c
@@ -0,0 +1,256 @@
+/*
+ * This work is part of the White Rabbit Node Core project.
+ *
+ * Copyright (C) 2013-2015 CERN (www.cern.ch)
+ * Author: Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+
+
+/*.
+ * WR Distributed DDS Realtime Firmware.
+ *
+ * rt-d3s.c: Main RT firnmware for the Distributed DDS system.
+ */
+
+#include <string.h>
+
+#include "wr-d3s-common.h"
+#include "wrtd-serializers.h"
+#include "rt-d3s.h"
+
+// state of our DDS loop
+struct dds_master_state master_loop;
+
+/* Forces a software reset of the DDS core. May be only run
+   AFTER the AD9516 has been programmed and locked */
+static void reset_core()
+{
+
+/* First, reset the DDS DAC serdes PLL */
+    dp_writel(DDS_RSTR_PLL_RST, DDS_REG_RSTR);
+    delay(1000);
+    dp_writel(0, DDS_REG_RSTR);
+    delay(1000);
+    while(! gpior_get(DDS_GPIOR_SERDES_PLL_LOCKED) );
+
+    dp_writel(DDS_RSTR_SW_RST, DDS_REG_RSTR); // trigger a software reset
+    delay(1000);
+    dp_writel(0, DDS_REG_RSTR);
+    delay(1000);
+
+    delay(1000000);
+}
+
+void init()
+{
+    ad9516_init();
+
+    /* Setup for 352 MHz RF reference: divider = 8 (44 MHz internal/DDS frequency) */
+    ad9510_init();
+
+    reset_core();
+
+	/* Set up the phase detector to work at ~15 MHz (44 MHz / 3) */
+    adf4002_configure(2,2,0);
+
+    /* all SVEC GPIOs->outputs */
+    gpio_set(24);
+    gpio_set(25);
+    gpio_set(26);
+
+    /* Set RF counter safe load phase (chosen experimentally) */
+    dp_writel( DDS_RF_RST_PHASE_LO_W(220) | DDS_RF_RST_PHASE_HI_W(30), DDS_REG_RF_RST_PHASE );
+
+    wr_init();
+
+    dbg_printf("RT_D3S firmware initialized.");
+}
+
+void setup_test_output(uint32_t tune_hi, uint32_t tune_lo)
+{
+    uint32_t pd_data;
+
+    dp_writel(tune_hi, DDS_REG_FREQ_HI);
+    dp_writel(tune_lo, DDS_REG_FREQ_LO);
+
+    dp_writel(DDS_CR_SAMP_EN | DDS_CR_SAMP_DIV_W(100 - 1), DDS_REG_CR );
+
+    dp_writel(0, DDS_REG_ACC_LOAD_HI);
+    dp_writel(0, DDS_REG_ACC_LOAD_LO);
+
+    int sample_idx;
+
+// ensure we're starting at a known TAI time to keep test signals
+// generated on multiple devices synchronized as long as they are locked
+// to WR
+    do {
+	       while(!dds_poll_next_sample(&pd_data));
+
+	          sample_idx = dp_readl(DDS_REG_SAMPLE_IDX);
+    } while ( sample_idx != 0);
+
+
+    dp_writel( DDS_TUNE_VAL_LOAD_ACC, DDS_REG_TUNE_VAL);
+
+
+}
+
+/*
+
+Host control
+
+*/
+
+/* Sends an acknowledgement reply */
+static inline void ctl_ack( uint32_t seq )
+{
+    struct wrnc_msg buf = hmq_msg_claim_out (WR_D3S_OUT_CONTROL, 16);
+    uint32_t id_ack = WR_D3S_REP_ACK_ID;
+
+    wrnc_msg_header (&buf, &id_ack, &seq);
+    hmq_msg_send (&buf);
+}
+
+/* Sends an error reply */
+static inline void ctl_err( uint32_t seq, int err )
+{
+    struct wrnc_msg buf = hmq_msg_claim_out (WR_D3S_OUT_CONTROL, 16);
+    uint32_t id_ack = WR_D3S_REP_NACK;
+
+    wrnc_msg_header (&buf, &id_ack, &seq);
+    wrnc_msg_int32 ( &buf, &err );
+    hmq_msg_send (&buf);
+}
+
+static inline void ctl_d3s_start_response_logging (uint32_t seq, struct wrnc_msg *ibuf)
+{
+    int n_samples;
+
+    wrnc_msg_int32 (ibuf, &n_samples);
+    ctl_ack(seq);
+    resp_log_start(&master_loop.rsp_log, seq, n_samples);
+}
+
+static inline void ctl_d3s_stream_config (uint32_t seq, struct wrnc_msg *ibuf)
+{
+    int mode;
+
+    wrnc_msg_int32(ibuf, &mode);
+    wrnc_msg_int32(ibuf, &master_loop.stream_id);
+    wrnc_msg_int64(ibuf, &master_loop.base_freq);
+
+    master_loop.enabled = (mode != D3S_STREAM_MASTER ) ? 1 : 0;
+
+    pp_printf("StreamConfig: mode %d enabled %d\n", mode, master_loop.enabled );
+
+    if( master_loop.enabled )
+        dds_master_start(&master_loop);
+    else
+        dds_master_stop(&master_loop);
+
+    ctl_ack (seq);
+}
+
+static inline void ctl_d3s_test_signal (uint32_t seq, struct wrnc_msg *ibuf)
+{
+    uint64_t base_tune;
+
+    wrnc_msg_int64(ibuf, &base_tune);
+
+    uint32_t tune_hi = (base_tune >> 32) & 0xffffffff;
+    uint32_t tune_lo = (base_tune >> 0) & 0xffffffff;
+
+    setup_test_output (tune_hi, tune_lo);
+
+// disable the loop so it doesn't mess up the test signal
+    dds_master_stop( &master_loop );
+
+    ctl_ack (seq);
+}
+
+static inline void ctl_d3s_set_gains (uint32_t seq, struct wrnc_msg *ibuf)
+{
+    wrnc_msg_int32(ibuf, &master_loop.kp);
+    wrnc_msg_int32(ibuf, &master_loop.ki);
+    wrnc_msg_int32(ibuf, &master_loop.vco_gain);
+
+    dp_writel(master_loop.vco_gain, DDS_REG_GAIN);
+
+    ctl_ack (seq);
+}
+
+static inline void ctl_d3s_ping (uint32_t seq, struct wrnc_msg *ibuf)
+{
+    pp_printf("ping[%d]\n", seq);
+    ctl_ack(seq);
+}
+
+
+/* Receives command messages and call matching command handlers */
+static inline void do_control()
+{
+    uint32_t cmd, seq;
+    uint32_t p = mq_poll();
+
+    /* HMQ control slot empty? */
+    if(! ( p & ( 1<< WR_D3S_IN_CONTROL )))
+        return;
+
+    struct wrnc_msg ibuf = hmq_msg_claim_in (WR_D3S_IN_CONTROL, 16);
+
+    wrnc_msg_header(&ibuf, &cmd, &seq);
+
+#define _CMD(id, func)          \
+    case id:                    \
+    {                           \
+        func(seq, &ibuf);       \
+        break;                  \
+    }
+
+    switch(cmd)
+    {
+    _CMD(WR_D3S_CMD_START_RESPONSE_LOGGING,  ctl_d3s_start_response_logging)
+    _CMD(WR_D3S_CMD_TEST_SIGNAL,             ctl_d3s_test_signal)
+    _CMD(WR_D3S_CMD_STREAM_CONFIG,           ctl_d3s_stream_config)
+    _CMD(WR_D3S_CMD_PING,                    ctl_d3s_ping)
+    _CMD(WR_D3S_CMD_SET_GAINS,               ctl_d3s_set_gains)
+    default:
+          break;
+    }
+
+    /* Drop the message once handled */
+    mq_discard(0, WR_D3S_IN_CONTROL);
+}
+
+void main_loop()
+{
+    dds_master_init ( &master_loop );
+    dds_master_start (&master_loop);
+
+    int n = 0;
+
+    for(;;)
+    {
+        dds_master_update (&master_loop);
+        wr_update_link();
+        do_control();
+
+        if(n == 100000)
+        {
+            pp_printf("CT: %d\n", master_loop.current_tune);
+            n=0;
+        }
+        n++;
+    }
+}
+
+
+int main()
+{
+	init();
+    main_loop();
+
+	return 0;
+}
---
+++ applications/wr_d3s/rt/master_pd/master_loop.c
@@ -0,0 +1,323 @@
+/*
+ * This work is part of the White Rabbit Node Core project.
+ *
+ * Copyright (C) 2013-2015 CERN (www.cern.ch)
+ * Author: Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+
+
+/*.
+ * WR Distributed DDS Realtime Firmware.
+ *
+ * master_loop.c - master side PLL
+ */
+
+#include <string.h>
+
+#include "wr-d3s-common.h"
+#include "wrtd-serializers.h"
+#include "rt-d3s.h"
+
+
+/*
+
+PLL response logging code
+
+*/
+
+// initializes the PLL response log
+static void resp_log_init(struct resp_log_state *state)
+{
+    state->active = 0;
+}
+
+// adds a new phase-control value entry to the response log, send if full
+void resp_log_update(struct resp_log_state *state, int phase, int y)
+{
+    if(!state->active)
+        return;
+
+    state->buf[state->block_offset++] = phase;
+    state->buf[state->block_offset++] = y;
+
+    state->remaining_samples--;
+
+    if(state->block_offset == state->block_samples || !state->remaining_samples)
+    {
+        struct wrnc_msg buf = hmq_msg_claim_out (WR_D3S_OUT_CONTROL, RESP_LOG_BUF_SIZE + 3);
+
+        uint32_t id = WR_D3S_REP_LOG_PAYLOAD;
+        int i;
+
+        wrnc_msg_header (&buf, &id, &state->seq);
+        wrnc_msg_int32 (&buf, (int*)&state->block_offset);
+        wrnc_msg_int32 (&buf, (int*)&state->block_index);
+
+        for(i = 0; i < state->block_offset; i++)
+            wrnc_msg_int32(&buf, (int*)&state->buf[i]);
+
+        hmq_msg_send (&buf);
+
+        state->block_offset = 0;
+        state->block_index++;
+    }
+
+    if(state->remaining_samples == 0)
+    {
+        state->active = 0;
+    }
+}
+
+// starts response logging
+void resp_log_start( struct resp_log_state *state, int seq, int n_samples )
+{
+    state->block_index = 0;
+    state->seq = seq;
+    state->block_offset = 0;
+    state->block_samples = RESP_LOG_BUF_SIZE;
+    state->remaining_samples = n_samples;
+    state->active = 1;
+}
+
+/*
+
+Message handling
+
+*/
+
+static inline volatile struct wr_d3s_remote_message* prepare_message(struct dds_master_state *state, int type )
+{
+    volatile struct wr_d3s_remote_message *msg = mq_map_out_buffer(1, WR_D3S_REMOTE_OUT_STREAM);
+
+    mq_claim(1, WR_D3S_REMOTE_OUT_STREAM);
+
+    msg->hdr.target_ip = 0xffffffff;    /* broadcast */
+    msg->hdr.target_port = 0xebd0;      /* port */
+    msg->hdr.target_offset = 0x4000;    /* target EB slot */
+
+    /* Embed transmission time for latency measyurement */
+    msg->transmit_seconds = lr_readl(WRN_CPU_LR_REG_TAI_SEC);
+    msg->transmit_cycles = lr_readl(WRN_CPU_LR_REG_TAI_CYCLES);
+    msg->type = type;
+    msg->stream_id = state->stream_id;
+    msg->sampling_divider = state->sampling_divider;
+    msg->lock_id = state->lock_id;
+
+    return msg;
+}
+
+static inline void send_phase_fixup( struct dds_master_state *state )
+{
+    volatile struct wr_d3s_remote_message *msg = prepare_message ( state, D3S_MSG_PHASE_FIXUP );
+
+    msg->phase_fixup.base_freq = state->base_freq;
+    msg->phase_fixup.vco_gain = state->vco_gain;
+
+    msg->phase_fixup.fixup_tai = state->fixup_time.seconds;
+    msg->phase_fixup.fixup_phase = state->fixup_phase;
+
+    msg->phase_fixup.rf_cnt_period = state->rf_cnt_period;
+    msg->phase_fixup.rf_cnt_snap_cycles = state->rf_cnt_snap_timestamp.ticks;
+    msg->phase_fixup.rf_cnt_snap_count  = state->rf_cnt_snap_count;
+
+    pp_printf("SendFixup [%d]!\n", state->stats.sent_updates);
+
+
+    mq_send(1, WR_D3S_REMOTE_OUT_STREAM, 32);
+
+    state->stats.sent_fixups++;
+}
+
+static inline void send_tune_update( struct dds_master_state *state, struct wr_timestamp *ts )
+{
+    volatile struct wr_d3s_remote_message *msg = prepare_message ( state, D3S_MSG_TUNE_UPDATE );
+
+    msg->tune_update.tai = ts->seconds;
+    msg->tune_update.tune = state->current_tune;
+    msg->tune_update.sample_id = state->current_sample_idx;
+
+    mq_send(1, WR_D3S_REMOTE_OUT_STREAM, 32);
+
+    state->stats.sent_updates++;
+}
+
+
+/*
+
+Master PLL servo
+
+*/
+
+static int pi_update(struct dds_master_state *state, int phase_error)
+{
+    int err = -(phase_error - 32768);
+
+    state->integ += err;
+
+
+    int y = (state->kp * err + state->ki * state->integ) >> 14;
+
+    resp_log_update(&state->rsp_log, phase_error, y);
+
+    if( !state->locked )
+    {
+        if(  abs(err) <= state->lock_threshold)
+        {
+            state->lock_counter++;
+            if(state->lock_counter == state->lock_samples)
+            {
+        		dbg_printf("[master] RF lock acquired\n");
+                state->locked = 1;
+                state->lock_id++;
+                state->lock_counter = 0;
+            }
+        }
+    } else { // locked==1
+        if(  abs(err) > state->lock_threshold)
+        {
+            state->lock_counter++;
+            if(state->lock_counter == state->delock_samples)
+            {
+		        dbg_printf("[master] RF lock lost\n");
+                state->locked = 0;
+                state->lock_counter = 0;
+            }
+
+        }
+    }
+
+
+    return y;
+}
+
+int dds_poll_next_sample(uint32_t *pd_data)
+{
+	uint32_t v;
+    v = dp_readl(DDS_REG_PD_DATA);
+    if (!( v & DDS_PD_DATA_VALID))
+        return 0;
+
+    *pd_data = v;
+
+    dp_writel(0, DDS_REG_PD_DATA);
+
+    return 1;
+}
+
+static uint64_t get_phase_snapshot(struct dds_master_state *state)
+{
+    uint32_t snap_lo = dp_readl ( DDS_REG_ACC_SNAP_LO );
+    uint32_t snap_hi = dp_readl ( DDS_REG_ACC_SNAP_HI );
+
+    int64_t acc_snap = snap_lo;
+    acc_snap |= ((int64_t) snap_hi ) << 32;
+
+    acc_snap += (int64_t)DDS_SNAP_LAG_SAMPLES * (state->base_freq + state->current_tune) ;
+    acc_snap &= DDS_ACC_MASK;
+
+    return acc_snap;
+}
+
+int dds_master_update(struct dds_master_state *state)
+{
+	uint32_t pd_data;
+
+    if (!wr_is_timing_ok())
+        return 0;
+
+    if(!dds_poll_next_sample(&pd_data))
+    	return 0;
+
+    state->last_adc_value = -((pd_data & 0xffff)    - 32768);
+
+    // produce timestamp of the current tune sample
+    state->current_sample_idx = dp_readl(DDS_REG_SAMPLE_IDX);
+    struct wr_timestamp ts;
+
+    ts.seconds = lr_readl(WRN_CPU_LR_REG_TAI_SEC);
+    ts.ticks = state->current_sample_idx * state->sampling_divider * 125;
+
+    /* do the actual PLL's job */
+    int y = pi_update(state, pd_data & 0xffff);
+
+    dp_writel(y & 0xffffff, DDS_REG_TUNE_VAL);
+
+	state->current_tune = y;//& 0xffffff;
+
+    if(!state->locked)
+        return 0;
+
+    if(state->current_sample_idx == 0)
+    {
+        // take snapshot of the RF phase
+        state->fixup_time  = ts;
+        state->fixup_phase = get_phase_snapshot(state);
+        state->fixup_ready = 1;
+
+        // and the RF counter
+        state->rf_cnt_snap_count = dp_readl(DDS_REG_RF_CNT_RF_SNAPSHOT);
+		state->rf_cnt_snap_timestamp.ticks = dp_readl(DDS_REG_RF_CNT_CYCLES_SNAPSHOT);
+        state->rf_cnt_snap_timestamp.seconds = lr_readl(WRN_CPU_LR_REG_TAI_SEC);
+
+        send_phase_fixup(state);
+    }
+
+    send_tune_update(state, &ts);
+
+
+	return 1;
+}
+
+
+void dds_master_init (struct dds_master_state *state)
+{
+    state->enabled = 0;
+    state->kp = 200000;//100 << 4;
+    state->ki = 1024; //1
+    //state->kp = 100 << 4;
+    //state->ki = 1;
+    state->lock_samples = 10000;
+    state->delock_samples = 1000;
+    state->lock_threshold = 2000;
+    state->sampling_divider = 100;
+    state->vco_gain = 1024;
+    state->stream_id = 123;
+    state->lock_id = 0;
+
+    state->rf_cnt_period = 44 * 1000 * 1000; // 44 MHz by default
+    state->base_freq=0x2d0e5604189ULL; // 352 MHz by default
+
+}
+
+void dds_master_start(struct dds_master_state *state)
+{
+    state->enabled = 1;
+    state->integ = 0;
+    state->locked = 0;
+    state->lock_counter = 0;
+    state->fixup_ready = 0;
+
+    resp_log_init(&state->rsp_log);
+
+    dp_writel(0, DDS_REG_CR);
+
+    /* set DDS center frequency */
+    dp_writel(DDS_CR_SAMP_DIV_W(state->sampling_divider - 1), DDS_REG_CR);
+    dp_writel(state->base_freq >> 32, DDS_REG_FREQ_HI);
+    dp_writel(state->base_freq & 0xffffffff, DDS_REG_FREQ_LO);
+    dp_writel(state->vco_gain, DDS_REG_GAIN);
+    dp_writel(state->rf_cnt_period - 1, DDS_REG_RF_CNT_PERIOD);
+    delay(100);
+
+    /* Start sampling */
+    dp_writel(DDS_CR_SAMP_EN | DDS_CR_SAMP_DIV_W(state->sampling_divider - 1), DDS_REG_CR);
+}
+
+
+void dds_master_stop(struct dds_master_state *state)
+{
+    dp_writel(0, DDS_REG_CR);
+    state->enabled = 0;
+}
